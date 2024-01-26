0873b059bb4a031af80e3f85b486c2701abbb469
JAMBON Yoan <yoanjambon21@macbook-air-de-jambon.home>
2024-01-26 16:35:47 +0100

    correc supp

--- js/darklightmode.js
+++ js/darklightmode.js
@@ -9,8 +9,12 @@ let shadow = document.querySelectorAll(".boutonRose, .boutonTry");
 let logo = document.querySelector(".logoHeader");
 
 function checkValue() {
+  const container = document.body;
+  const dataTheme = container.getAttribute("data-theme");
+  let theme = localStorage.getItem("data-theme");
   if (checkboxValue === "off") {
     checkboxValue = "on";
+    container.setAttribute("data-theme", "light");
     body.classList.remove("dark");
     header.classList.remove("darkHeader");
     titles.forEach((element) => {
@@ -23,8 +27,10 @@ function checkValue() {
       element.classList.remove("darkButton");
     });
     logo.classList.remove("darkLogo");
+    localStorage.toggled("data-theme", "light");
   } else {
     checkboxValue = "off";
+    container.setAttribute("data-theme", "dark");
     body.classList.add("dark");
     header.classList.add("darkHeader");
     titles.forEach((element) => {
@@ -37,22 +43,23 @@ function checkValue() {
       element.classList.add("darkButton");
     });
     logo.classList.add("darkLogo");
+    localStorage.setItem("data-theme", "dark");
   }
 }
 
-function changerLangue() {
-  var selectElement = document.getElementById("langSelector");
-  var langueSelectionnee = selectElement.value;
+// function changerLangue() {
+//   var selectElement = document.getElementById("langSelector");
+//   var langueSelectionnee = selectElement.value;
 
-  // Masquer tous les éléments de texte
-  var textes = document.getElementsByClassName("texte");
-  for (var i = 0; i < textes.length; i++) {
-    textes[i].style.display = "none";
-  }
+//   // Masquer tous les éléments de texte
+//   var textes = document.getElementsByClassName("texte");
+//   for (var i = 0; i < textes.length; i++) {
+//     textes[i].style.display = "none";
+//   }
 
-  // Afficher uniquement le texte correspondant à la langue sélectionnée
-  var texteLangue = document.getElementsByClassName(langueSelectionnee);
-  for (var i = 0; i < texteLangue.length; i++) {
-    texteLangue[i].style.display = "block";
-  }
-}
+//   // Afficher uniquement le texte correspondant à la langue sélectionnée
+//   var texteLangue = document.getElementsByClassName(langueSelectionnee);
+//   for (var i = 0; i < texteLangue.length; i++) {
+//     texteLangue[i].style.display = "block";
+//   }
+// }
